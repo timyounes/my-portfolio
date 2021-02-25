9049cd072eecf80232bd189dbe048d46aaa54a78
Adhan Timothy Younes <adhantimothyyounes@gmail.com>
2021-02-25 23:04:01 +0800

    Updates

---
+++ precache-manifest.a00f6caef282769bab90205f7ded0004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2d48905f25ea2b456ed99c867f891c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7fb8ad0637007f0f8704",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60e7228853e85d3ce392",
+    "url": "/my-portfolio/static/js/2.eb496acd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-portfolio/static/js/2.eb496acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb8ad0637007f0f8704",
+    "url": "/my-portfolio/static/js/main.286f850a.chunk.js"
+  },
+  {
+    "revision": "7c7d8426a5976bd5dcaf",
+    "url": "/my-portfolio/static/js/runtime-main.ec1bad13.js"
+  }
+]);
